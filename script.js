18bd5cf19ce186f3b9ce864eccc8feaa933b68a6
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-01-21 21:04:51 +0200

    added classes to total time by the value, and added comments to js file

--- script.js
+++ script.js
@@ -70,26 +70,49 @@ let task10 = {
     tasksFinished: 12,
     topic: 'Surfing'
 };
+//array of tasks, all tasks are objects
 let arrOfTasks = [task1, task2, task3, task4, task5, task6, task7, task8, task9, task10];
 
 //Calculate and add total time of work and finished precent properties to each object
 for (let task of arrOfTasks) {
-    task['totalTime'] = (task.finishedAt - task.startedAt) / 3600000 + ' hours';
+    task['totalTime'] = (task.finishedAt - task.startedAt) / 3600000;
     task['tasksFinishedPrecent'] = Math.floor(((task.tasksFinished / task.tasksGiven) *100)) + '%';
     task.startedAt = task.startedAt.toLocaleTimeString(); //saved only the local time
     task.finishedAt = task.finishedAt.toLocaleTimeString(); //saved only the local time
 }
+
+// add array of headers to the table
 let arrOfHeaderTable =  ['Started At', 'Finished At', 'Ttasks Given', 'Tasks Finished', 'Topic', 'Total Time', 'Tasks Finished Precent'];
 document.write(`<table>`);
+
+// for loop to create the headers of the table
 document.write(`<tr>`);
 for (let head of arrOfHeaderTable) {
     document.write(`<th>${head}</th>`);
 }
+//create the table body and put all the same property value in same row
+//check if the property is totalTime to give him a class by the value
 document.write(`</tr>`);
-for (let task of arrOfTasks) {
+for (let tasks of arrOfTasks) {
     document.write(`<tr>`);
-    for (let value in task) {
-        document.write(`<td>${task[value]}</td>`);
+    for (let prop in tasks) {
+        if (prop === 'totalTime') {
+            if (tasks[prop] <= 2) {
+                let className = 'total-time-good';
+                document.write(`<td class="${className}">${tasks[prop]}</td>`);
+            }
+            if (tasks[prop] > 2 && tasks[prop] <= 5) {
+                let className = 'total-time-avg';
+                document.write(`<td class="${className}">${tasks[prop]}</td>`);
+            }
+            if (tasks[prop] > 5) {
+                let className = 'total-time-bad';
+                document.write(`<td class="${className}">${tasks[prop]}</td>`);
+            }
+
+        } else {
+            document.write(`<td>${tasks[prop]}</td>`);
+        }
     }
     document.write(`</tr>`);
 }
